779aa95585541071ad14083203dd174ba38c1ce6
gknanhe <cse18gknanhejd@gmail.com>
2023-09-25 08:51:27 +0530

    fixed API not working

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
+// const API_ROOT = 'https://codeial.codingninjas.com:8000/api/v2/';
 const API_ROOT = 'https://codeial.codingninjas.com:8000/api/v2/';
-
 // doc url - https://www.notion.so/aakashcn/Codeial-API-docs-3a4d0b5a42c54f0a94d951a42aabc13f
 export const API_URLS = {
   login: () => `${API_ROOT}/users/login`,
